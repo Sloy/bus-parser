4994c53fb55cef7930c4c68f7135d7574ed8b3e1
Rafa Vázquez <rafa.vazsan@gmail.com>
2017-06-08 22:53:56 +0200

    Add script to automaticall update public database

---
+++ updateData.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e # Stop script on error
+set -x # Print the commands below
+
+GIT_MAIN_BRANCH="master"
+GIT_REMOTE="origin"
+OUTPUT_DIR="../busparser-data"
+JAR_FILE="busparser-core/build/libs/busparser-core-all-1.0.jar"
+DATE=$(date +%d-%m-%Y" "%H:%M:%S);
+DATA_VERSION=$(date +%Y%m%d%H%M);
+PUBLIC_FILES_URL="https://gitlab.com/Sloy/sevibus-data/raw/master/sql/"
+
+# Build the code
+./gradlew fatJar
+
+# Run the jar's main task to generate outputs
+java -jar $JAR_FILE
+
+
+cd $OUTPUT_DIR
+# Create info json
+
+echo "{
+    \"data_version\": $DATA_VERSION,
+    \"app_min_build_version\": 94,
+    \"data_location\": {
+        \"paradas\": \"${PUBLIC_FILES_URL}paradas.sql\",
+        \"lineas\": \"${PUBLIC_FILES_URL}lineas.sql\",
+        \"relaciones\": \"${PUBLIC_FILES_URL}relaciones.sql\",
+        \"secciones\": \"${PUBLIC_FILES_URL}secciones.sql\",
+        \"tipolineas\": \"${PUBLIC_FILES_URL}tipolineas.sql\"
+    }
+}" > info.json
+
+git add .
+git ci -m "Dump on $DATE"
+git push
