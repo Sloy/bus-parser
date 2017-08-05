24b8aa4fd27d48ac776e92d2d8afe336b524512e
Rafa Vázquez <rafa.vazsan@gmail.com>
2017-08-05 16:59:18 +0200

    Fix files url and improve script

--- updateData.sh
+++ updateData.sh
@@ -5,24 +5,25 @@ set -x # Print the commands below
 GIT_MAIN_BRANCH="master"
 GIT_REMOTE="origin"
 OUTPUT_DIR="../busparser-data/"
-JAR_FILE="busparser-core/build/libs/busparser-core-all-1.0.jar"
+JAR_FILE="$(pwd)/busparser-core/build/libs/busparser-core-all-1.0.jar"
 DATE=$(date +%d-%m-%Y" "%H:%M:%S);
 DATA_VERSION=$(date +%Y%m%d%H%M);
-PUBLIC_FILES_URL="https://gitlab.com/Sloy/sevibus-data/raw/master/sql/"
-
-# Update the output data
-git submodule update --init --recursive
+PUBLIC_FILES_URL="https://raw.githubusercontent.com/Sloy/sevibus-data/master/sql/"
 
 # Build the code
 ./gradlew fatJar
+
+# Change to output dir and prepare the working tree
+cd $OUTPUT_DIR
+git co master
+git pull origin master
+git co -b $DATA_VERSION
 
 # Run the jar's main task to generate outputs
 java -jar $JAR_FILE $OUTPUT_DIR
 
 
-cd $OUTPUT_DIR
 # Create info json
-
 echo "{
     \"data_version\": $DATA_VERSION,
     \"app_min_build_version\": 94,
@@ -35,7 +36,6 @@ echo "{
     }
 }" > info.json
 
-git co -b $DATA_VERSION
 git add .
 git ci -m "Dump on $DATE"
 git pushr
